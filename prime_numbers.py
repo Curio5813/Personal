e5e77c2f32268f850d6a007776ecd107500f5931
mfg0914 <marcosglasner@gmail.com>
2022-07-25 16:15:27 +0100

    Updating and Refactoring files.

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,12 +1,18 @@
-def gerarPrimos():
-  lim = 700_000 + 1
-  p = []
-  n_p = set()
-  for i in range(2, lim):
-    if i in n_p:
-      continue
-    for k in range(i * 2, lim, i):
-      n_p.add(k)
-    p.append(i)
-  return p
+def generatePrimeNumbers():
+    """
+    This function give a list of prime numbers until a
+    limit setted.
+    :return:
+    """
+    p, n_p, lim = [], set(), 1_000_000 + 1
+    for i in range(2, lim):
+        if i in n_p:
+            continue
+        for k in range(i * 2, lim, i):
+            n_p.add(k)
+        p.append(i)
+    print(len(p))
+    return print(*p)
 
+
+generatePrimeNumbers()
