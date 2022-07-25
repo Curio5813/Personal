c4f024d8d65de44f9477d390fc1c1827f05a398d
mfg0914 <marcosglasner@gmail.com>
2022-07-25 01:40:00 +0100

    Updating and Refactoring files.

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,19 +1,39 @@
-"""
-Faça um programa que dado um número criar o Triangulo de Pascal.
-"""
+from math import factorial
 
 
-def binomio_de_newton(n=int(input('Digite um número igual ou maior que 1: '))):
-    fat_n = 1
-    fat_k = 1
-    k = 0
-    while n <= 0:
-        print('')
-        print('Número inválido!')
-        n = int(input('Digite um número igual ou maior que 1: '))
-    while n >= 1:
-        fat_n *= n
-        n -= 1
+def newtonBinomial():
+    """
+    This fucntion create an of list the Newton's Binomial, until the
+    values of numbers of terms given.
+    """
+    num = int(input("How many terms will have at the Newton's Binomial: "))
+    p_t, fat, cont, t = [], [], 0, num
+    for i in range(num + 1):
+        for k in range(num + 1):
+            if t < 0:
+                break
+            binome = int(factorial(num) / (factorial(num - cont) * factorial(cont)))
+            fat.append(binome)
+            cont += 1
+        p_t.append(fat)
+        num -= 1
+        fat = []
+        cont = 0
+        t -= 1
+    p_t.reverse()
+    return p_t
 
 
+def pascalTriangle(p_t):
+    """
+    This function print the Pascal's Triangle.
+    :param p_t:
+    :return:
+    """
+    for i in range(0, len(p_t)):
+        for k in range(0, len(p_t[i])):
+            print(f"{(p_t[i][k]):>3}", end=' ')
+        print('\n', end='')
 
+
+pascalTriangle(newtonBinomial())
