6b5afe7d403714b769a83bb3004cb191c67e7ca3
Marcos Glasner <marcosglasner@gmial.com>
2023-01-13 22:46:54 +0000

    Updating file find_hypotenuse.py

--- find_hypotenuse.py
+++ find_hypotenuse.py
@@ -1,17 +1,17 @@
-from math import sqrt
+from math import hypot
 
 
-def acharHipotenusa():
+def findHypotenuse():
     """
-    Esta função retorna o valor da hipotenusa de um triangulo
-    retângulo, dados os dois catetos.
+    this function returns the value of the hypotenuse knowing the
+    values of the legs of a right triangle.
     :return:
     """
-    a = float(input('Cateto A: '))
-    b = float(input('Cateto B: '))
-    c = sqrt(a ** 2 + b ** 2)
+    a = float(input('Side A: '))
+    b = float(input('Side B: '))
+    c = hypot(a, b)
     print('')
-    return print(f'Hipotenusa: {c:.1f}')
+    return print(f'Hypotenuse = {c:.2f}')
 
 
-acharHipotenusa()
+findHypotenuse()
