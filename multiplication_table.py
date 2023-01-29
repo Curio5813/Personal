99717d27050cc15bc7bfc49bc5557dd4a683a52f
Marcos Glasner <marcosglasner@gmail.com>
2023-01-29 13:57:16 +0000

    Adding mltiplication_table.py file to the porject

---
+++ multiplication_table.py
@@ -0,0 +1,14 @@
+def multiplicationTable():
+    """
+    This function print a multiplication table for a number given by
+    user from 1 to 10.
+    :return:
+    """
+    number = int(input("which number do you want to see the multiplication table: "))
+    for i in range(1, 10 + 1):
+        print(f"{number} x {i:>2} = {(number * i):>2}")
+    print("")
+    print("This is all folks!")
+
+
+multiplicationTable()
