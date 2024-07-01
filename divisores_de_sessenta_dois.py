0be56208d2e35fa186e11d0bbd744fb9a0ee158e
Marcos Glasner <marcosglasner@gmail.com>
2024-07-01 18:13:32 +0100

    Updating program divisores_de_sessenta_dois.py

--- divisores_de_sessenta_dois.py
+++ divisores_de_sessenta_dois.py
@@ -1,10 +1,11 @@
 def divisores_de_sessenta_dois():
     """
     Verifications about the number 62 and it's implications
-    as the biggest gap between twin prime numbers.
+    as the biggest gap between twin prime numbers and the next
+    prime number not twin prime.
     :return:
     """
-    primos, n, cont, modular = [], 62, 1, []
+    primos, n, cont, modular, diffs = [], 62, 1, [], []
     for i in range(2, 10000 + 1):
         for j in range(2, 10000 + 1):
             if i % j == 0 and i == j:
@@ -20,7 +21,13 @@ def divisores_de_sessenta_dois():
             mod = n % primos[i]
             if mod not in primos:
                 modular.append(mod)
-    print(*modular)
+    for i in range(0, len(modular)):
+        if i >= len(modular) - 1:
+            break
+        diff = modular[i + 1] - modular[i]
+        diffs.append(abs(diff))
+    print(max(diffs))
+    print(*diffs)
 
 
 divisores_de_sessenta_dois()
