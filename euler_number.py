55a0fa7e3f410abb994eb8ced555dc993af28baf
Marcos Glasner <marcosglasner@gmail.com>
2024-07-06 20:34:13 +0100

    Updating the program euler_number.py to the repository.

--- euler_number.py
+++ euler_number.py
@@ -1,13 +1,18 @@
-def eulerNumber():
+from math import sqrt
+
+
+def euler_number():
     """
     This function calculates the Euler number, a transcendental number
     used in calculating exponential equations and rate changes and which
-    is itself its derivative.
+    is itself its derivative. It's a divergente irracional number.
     :return:
     """
-    n = int(input("How precisely you want the number: "))
-    e = (1 + (1 / n)) ** n
-    print(e)
+    n = float(input("How precisely you want the number: "))
+    e_squared = (1 + (1 / sqrt(n * n * 3))) ** (sqrt(n * n * 3))
+    e_intger = (1 + (1 / n)) ** n
+    print(e_squared)
+    print(e_intger)
 
 
-eulerNumber()
+euler_number()
