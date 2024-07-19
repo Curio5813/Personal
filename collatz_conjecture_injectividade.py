34d6bae500fcaa8c13686139cd8a0478f6b5c493
Marcos Glasner <marcosglasner@gmail.com>
2024-07-19 02:25:11 +0100

    Adding program to the repository.

---
+++ collatz_conjecture_injectividade.py
@@ -0,0 +1,32 @@
+def collatz_conjecture_injectividade():
+    """
+    Collatz Conjecture é a injective function until its reach the value 2.
+    :return:
+    """
+    num = int(input("Digit a integer positive number: "))
+    par, pares, flag = [], [], 0
+    while num != 2:
+        if num % 2 == 0:
+            par.append(num)
+            num //= 2
+            par.append(num)
+            pares.append(par)
+            par = []
+        if num % 2 == 1:
+            par.append(num)
+            num = 3 * num + 1
+            par.append(num)
+            pares.append(par)
+            par = []
+    print(pares)
+    for i in range(0, len(pares)):
+        if pares.count(pares[i]) > 1:
+            print("A função não é injetora!")
+            flag = 1
+    if flag == 0:
+        print("A função é injetora!")
+
+
+
+
+collatz_conjecture_injectividade()
