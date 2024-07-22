b7167199b8d9dc8e8f5d6683b2c34dd2f634799f
Marcos Glasner <marcosglasner@gmail.com>
2024-07-22 17:01:32 +0100

    The program dizima_periodica.py still need updating but it's works clearly.

--- dizima_periodica.py
+++ dizima_periodica.py
@@ -14,7 +14,6 @@ def dizima_periodica():
         if len(periodo) == periodo.count(dizima[i]) == 3:
             maior = periodo
             flag = 1
-            break
         if len(periodo) >= 2 and dizima.count(periodo) >= 2:
             vezes += 1
             maior = periodo
@@ -35,16 +34,17 @@ def dizima_periodica():
         idx2 = len(inteiro1) + 1
         deslocamento = dizima.split(".")
         deslocamento[1] = periodo
-        if deslocamento[1] == periodo:
+        tentar = dizima.split(".")
+        verificacao = tentar[1]
+        if deslocamento[1] == periodo and deslocamento[1] == verificacao:
             potencia10_1 = 1
             potencia10_2 = 10
-            inteiro1 += "." + periodo
+            inteiro1 = deslocamento[0] + "." + periodo
             inteiro1 = float(inteiro1)
             inteiro2 = inteiro1 * 10
             # parte inteira do calculo.
             inteiro1 = int(inteiro1)
             inteiro2 = int(inteiro2)
-            print(inteiro1, inteiro2)
             # Calcula o numerador
             numerador = inteiro2 - inteiro1
             # Calcula o denominador
@@ -63,9 +63,42 @@ def dizima_periodica():
             potencia10_2 = 10 ** (idx2 - diff)
             denominador = potencia10_2 - potencia10_1
             # printa a geratriz da dízima
-        if denominador < 1:
-            print("O número é não computável.")
-        else:
+            print("A Geratriz é:")
+            print(f"{numerador}/{denominador}", end=" ")
+            # Busca por frações reduzidas
+        for i in range(2, 10_000 + 1):
+            if numerador % i == 0 and denominador % i == 0:
+                while numerador % i == 0 and denominador % i == 0:
+                    numerador //= i
+                    denominador //= i
+                    cont += 1
+    else:
+        idx = len(maior)
+        periodo = maior
+        periodo = periodo[::-1]
+        dizima = dizima[::-1]
+        inteiro1 = dizima.replace(".", "").split(periodo)
+        inteiro1 = inteiro1[0]
+        delocamento = dizima.split(".")
+        delocamento[1] = periodo
+        tentar = dizima.split(".")
+        verificacao = tentar[1][0:idx]
+        if delocamento[1] == verificacao:
+            diff_dizima = dizima.split(".")
+            diff_dizima = diff_dizima[0]
+            diff = len(inteiro1) - len(diff_dizima)
+            potencia10_1 = 10 ** diff
+            inteiro2 = inteiro1 + periodo
+            potencia10_2 = 10 ** (len(inteiro2) - len(inteiro1))
+            # print(inteiro1, inteiro2)
+            # parte inteira do calculo.
+            inteiro1 = int(inteiro1)
+            inteiro2 = int(inteiro2)
+            # Calcula o numerador
+            numerador = inteiro2 - inteiro1
+            # Calcula o denominador
+            denominador = potencia10_2 - potencia10_1
+            # printa a geratriz da dízima
             print("A Geratriz é:")
             print(f"{numerador}/{denominador}", end=" ")
             # Busca por frações reduzidas
@@ -75,42 +108,22 @@ def dizima_periodica():
                         numerador //= i
                         denominador //= i
                         cont += 1
-    else:
-        idx = len(maior)
-        idx = idx
-        periodo = maior
-        periodo = periodo[::-1]
-        dizima = dizima[::-1]
-        inteiro1 = dizima.replace(".", "").split(periodo)
-        inteiro1 = inteiro1[0]
-        delocamento = dizima.split(".")
-        if delocamento[1] == periodo:
-            potencia10_1 = 1
-            potencia10_2 = 10
-            inteiro1 += "." + periodo
-            inteiro1 = float(inteiro1)
-            inteiro2 = inteiro1 * 10
-            # parte inteira do calculo.
-            inteiro1 = int(inteiro1)
-            inteiro2 = int(inteiro2)
-            # Calcula o numerador
-            numerador = inteiro2 - inteiro1
-            # Calcula o denominador
-            denominador = potencia10_2 - potencia10_1
+            if denominador == 1:
+                print("ou", end=" ")
+                print(f"{numerador}")
+            else:
+                if cont > 0:
+                    print("ou", end=" ")
+                    print(f"{numerador}/{denominador}")
         else:
-            inteiro1 = dizima.replace(".", "").split(periodo)
-            inteiro1 = inteiro1[0]
-            # Acha o tamanho da dízima
-            potencia10_2_n = dizima.split(".")
-            potencia10_2_n = potencia10_2_n[1].split(periodo)
-            # Conta quantas casas decimais existem depois da parte inteira e antes da dízima
-            potencia10_2_n = len(potencia10_2_n[0])
-            # Acha o inteiro até a primeira repetição da dízima
-            inteiro2 = dizima[0:-idx-1].replace(".", "")
-            # As duas linha abaixo acha o denominador da fração subtraindo as potências de 10 para cada
+            diff_dizima = dizima.split(".")
+            diff_dizima = diff_dizima[0]
+            diff = len(inteiro1) - len(diff_dizima)
+            potencia10_1 = 10 ** diff
+            inteiro2 = inteiro1 + periodo
+            potencia10_2 = 10 ** (diff + len(periodo))
+            # print(inteiro1, inteiro2)
             # parte inteira do calculo.
-            potencia10_1 = 10 ** potencia10_2_n
-            potencia10_2 = 10 ** (potencia10_2_n + idx + vezes)
             inteiro1 = int(inteiro1)
             inteiro2 = int(inteiro2)
             # Calcula o numerador
@@ -118,18 +131,15 @@ def dizima_periodica():
             # Calcula o denominador
             denominador = potencia10_2 - potencia10_1
             # printa a geratriz da dízima
-            if denominador < 1:
-                print("O número é não computável.")
-            else:
-                print("A Geratriz é:")
-                print(f"{numerador}/{denominador}", end=" ")
-                # Busca por frações reduzidas
-                for i in range(2, 10_000 + 1):
-                    if numerador % i == 0 and denominador % i == 0:
-                        while numerador % i == 0 and denominador % i == 0:
-                            numerador //= i
-                            denominador //= i
-                            cont += 1
+            print("A Geratriz é:")
+            print(f"{numerador}/{denominador}", end=" ")
+            # Busca por frações reduzidas
+            for i in range(2, 10_000 + 1):
+                if numerador % i == 0 and denominador % i == 0:
+                    while numerador % i == 0 and denominador % i == 0:
+                        numerador //= i
+                        denominador //= i
+                        cont += 1
             if denominador == 1:
                 print("ou", end=" ")
                 print(f"{numerador}")
