0d2fabf40e6636b0ecb63257327a6af1f0b7ccbd
Marcos Glasner <marcosglasner@gmail.com>
2025-11-21 09:55:50 +0000

    Adicionando o programa 'primos_gemeos.py' ao repositório.

---
+++ primos_gemeos.py
@@ -0,0 +1,36 @@
+def primos_gemeos():
+    """
+    Acha todos o números primos gêmeos até 10.000 e sua distância para o próximo primo
+    gêmeos ou não.
+    :return:
+    """
+    primos, gemeos, temp, distancias = [], [], [], []
+    for i in range(2, 100_000 + 1):
+        for j in range(2, 100_000 + 1):
+            if i % j == 0 and i != j:
+                break
+            if i % j == 0 and i == j:
+                primos.append(i)
+    for i in range(len(primos)):
+        for j in range(len(primos)):
+            if i != j and primos[i] - primos[j] == 2:
+                temp.append(primos[i])
+                temp.append(primos[j])
+                gemeos.append(temp)
+                temp = []
+    for i in range(len(gemeos)):
+        for j in range(len(primos)):
+            if gemeos[i][0] < primos[j]:
+                distancias.append(primos[j] - gemeos[i][0])
+                break
+    print("Primo 1 - Primo 2 -> Distância ao próximo número primo:")
+    for i in range(len(gemeos)):
+        print(f"{gemeos[i][1]:>5} -{gemeos[i][0]:>5} -> {distancias[i]}")
+    maior_idx = distancias.index(max(distancias))
+    print(*gemeos[maior_idx], end="")
+    print(f": Maior Distância = {distancias[maior_idx]}")
+
+
+
+if __name__ == '__main__':
+    primos_gemeos()
